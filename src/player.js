b96936d41e0258d28ba085a88d55a97bd8eb051e
Owen Lawson <owen@uken.com>
2011-09-23 17:12:10 -0400

    Start of initial connection, plus we can now host HTTP files

--- src/player.js
+++ src/player.js
@@ -6,3 +6,13 @@ Player.prototype.name = "";
 Player.prototype.side = 0;
 Player.prototype.score = 0;
 Player.prototype.soldiers = [];
+
+Player.prototype.connect = function(server, port) {
+  
+  
+  this.socket = io.connect('http://localhost');
+  socket.on('news', function (data) {
+    console.log(data);
+    socket.emit('my other event', { my: 'data' });
+  });
+}
