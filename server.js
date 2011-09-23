722dba837077aadaa1e702aaa9e055b0a146fa1e
Owen Lawson <owen@uken.com>
2011-09-23 18:00:10 -0400

    Fix server

--- server.js
+++ server.js
@@ -1,42 +1,36 @@
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
+//HTTP / WS Server for Trench Game
+
+var port = Number(process.argv[2]) || 1337;
+
+var app = require('http')
+  , io = require('socket.io')
   , fs = require('fs')
   , static = require('node-static');
 
-var ws_port = process.argv[2] || 1337;
-var http_port = process.argv[3] || 8080;
-
-
+//HTTP Hosting
 var file = new(static.Server)('.');
 
-require('http').createServer(function (request, response) {
+app = app.createServer(function (request, response) {
     request.addListener('end', function () {
         //
         // Serve files!
         //
         file.serve(request, response);
     });
-}).listen(8080);
+})
+io = io.listen(app)
+app.listen(port);
 
-app.listen(ws_port);
-console.log("Server started on port "+http_port+" and listening for websockets on port "+ws_port)
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
 
-    res.writeHead(200);
-    res.end(data);
-  });
-}
-
+//WebSocket Server
 io.sockets.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
     console.log(data);
   });
 });
+
+
+//Done
+console.log("Server started on port "+port);
