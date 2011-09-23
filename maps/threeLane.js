c27dbdfa88ca7732e061a30692bf3683ee8440e0
Adam Proctor <adam@uken.com>
2011-09-23 18:35:34 -0400

    three lane map

---
+++ maps/threeLane.js
@@ -0,0 +1,10 @@
+var threeLane = new TrenchMap({"id":"threeLane","tickets":"2000"}, [
+{"name":"left_spawn","type":"area","x":"0","y":"0","width":"128","height":"640","spawn":"p1"}, 
+{"name":"right_spawn","type":"area","x":"832","y":"0","width":"128","height":"640","spawn":"p2"}, 
+{"name":"top_lane","type":"area","x":"128","y":"0","width":"704","height":"176"}, 
+{"name":"top_lane","type":"area","x":"128","y":"464","width":"704","height":"176"}, 
+{"name":"mid_lane","type":"area","x":"240","y":"240","width":"480","height":"160"}, 
+{"name":"l_crossover","x":"128","y":"176","width":"112","height":"288"}, 
+{"name":"r_crossover","x":"720","y":"176","width":"112","height":"288"}
+]);
+
