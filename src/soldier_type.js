251ee1b17d25929fdd98fa08b0ae3f1adfc9945e
Mark Bolusmjak <mark.bolusmjak@uken.com>
2011-09-24 13:38:16 -0400

    detecting damage given. scaling (soldiers now scaled to 32x32)

--- src/soldier_type.js
+++ src/soldier_type.js
@@ -1,14 +1,23 @@
 var GenericSoldier = {
     radius : 20,
-    angle : 180
+    angle : 180,
+    damageAt : function(a, r){
+        return 20*(1-r/this.radius);
+    }
 };
 
 var Sniper = {
     radius : 60,
-    angle : 15
+    angle : 15,
+    damageAt : function(a, r){
+        return 20;
+    }
 };
 
 var MachineGunner = {
     radius : 140,
-    angle : 40
+    angle : 40,
+    damageAt : function(a, r){
+        return 20*(1.5-r/this.radius);
+    }
 };
