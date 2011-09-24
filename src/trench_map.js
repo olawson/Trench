b615110854d46541f04d862b4f2b0e1332205705
Adam Proctor <adam@uken.com>
2011-09-24 15:38:02 -0400

    truncate path to bounding boxes

--- src/trench_map.js
+++ src/trench_map.js
@@ -49,8 +49,12 @@ TrenchMap.prototype.filterPath = function(path) {
     var cur = path[i];
 
     //TODO filter path based on collision detection and redundancy
-
-    filteredPath.push(cur);
+    if(this.pointValid(cur.x, cur.y)) {
+      filteredPath.push(cur);
+    } else {
+      alert('invalid');
+      break;
+    }
     
     prev = cur;
   }
@@ -59,11 +63,31 @@ TrenchMap.prototype.filterPath = function(path) {
 };
 
 
+TrenchMap.prototype.pointValid = function(x, y) {
+  for(var i in this.areas) {
+    if(this.areas[i].containsPoint(x,y) == true) {
+      return true;
+    }
+  }
+  
+  return false;
+};
+
+
 
 
 
 /**
  * Areas for trench map
+ * cfg {
+     height: "640"
+     name: "left_spawn"
+     spawn: "p1"
+     type: "area"
+     width: "128"
+     x: "0"
+     y: "0
+ * }
  */
 function TrenchMapArea(config) {
   this.init(config);
@@ -76,6 +100,23 @@ TrenchMapArea.prototype.cfg = null;
  */
 TrenchMapArea.prototype.init = function(config) {
   this.cfg = config;
+  
+  this.cfg.x = parseInt(config.x);
+  this.cfg.y = parseInt(config.y);
+  this.cfg.width = parseInt(config.width);
+  this.cfg.height = parseInt(config.height);
+};
+
+TrenchMapArea.prototype.containsPoint = function(x,y) {
+  var minX = this.cfg.x;
+  var maxX = minX + this.cfg.width;
+  var minY = this.cfg.y;
+  var maxY = minY + this.cfg.height;
+  
+  var valid = (x >= minX && x <= maxX && y >= minY && y <= maxY);
+  console.log('Point valid?Area<'+this.cfg.name+'> <'+x+','+y+'> Area<'+minX+','+minY+','+maxX+','+maxY+'>, valid<'+valid+'>');
+  
+  return valid;
 };
 
 TrenchMapArea.prototype.render = function() {
