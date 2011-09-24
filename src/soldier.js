5c03bb92360131493ed80538e6e7d412bd4ba595
Mark Bolusmjak <mark.bolusmjak@uken.com>
2011-09-23 22:25:25 -0400

    fixes.

--- src/soldier.js
+++ src/soldier.js
@@ -1,9 +1,9 @@
 var ROTQ = 60; //seperate sprite rotations
 var SpriteSets = {
-    allies          : new Sprites("images/soldiers.png",64, 0,  64,64,60, ROTQ),
-    allies_selected : new Sprites("images/soldiers.png", 0, 0,  64,64,60, ROTQ),
-    axis            : new Sprites("images/soldiers.png",64,64,  64,64,60, ROTQ),
-    axis_selected   : new Sprites("images/soldiers.png",0 ,64,  64,64,60, ROTQ)
+    allies          : new Sprites("images/soldiers.png",64, 0,  64,64,ROTQ, 90),
+    allies_selected : new Sprites("images/soldiers.png", 0, 0,  64,64,ROTQ, 90),
+    axis            : new Sprites("images/soldiers.png",64,64,  64,64,ROTQ, 90),
+    axis_selected   : new Sprites("images/soldiers.png",0 ,64,  64,64,ROTQ, 90)
 };
 
 //soldier side is 1 (allies) or 2 (axis)
@@ -19,6 +19,7 @@ Soldier.prototype.y = 0;
 Soldier.prototype.dead = true;
 
 Soldier.prototype.direction = 45;
+Soldier.prototype.sprites = SpriteSets.allies;
 
 
 Soldier.prototype.render = function(context){
@@ -26,17 +27,19 @@ Soldier.prototype.render = function(context){
     context.beginPath();
     //translate to soldier-center = 0,0
     context.translate(this.x,this.y);
+
+    this.sprites.renderToContextWithAngle(context, this.direction)
     //draw center based stuff
     context.arc(0,0, this.radius, 0, Math.PI*2);
 
     context.moveTo(0,0);
     var theta = this.direction * Math.PI/180;
-    context.lineTo(Math.sin(theta)*this.radius, Math.cos(theta)*this.radius)
+    context.lineTo(Math.cos(theta)*this.radius, Math.sin(theta)*this.radius)
     context.stroke();
 
     context.closePath();
     context.restore();
-}
+};
 
 
 Soldier.prototype.setPath = function(newPath) {
@@ -71,30 +74,30 @@ function Sprites(file, x, y, width, height, rotations, space){
     var cxt = c.getContext("2d");
 
     var img = new Image();
-    img.src = file;
-
-    var self = this;
-
-    cxt.drawImage(img, x, y, width, height, 0,0,width,height)
+    img.onload = function(){
+        cxt.drawImage(img, x, y, width, height, 0,0,width,height)
     for (var r = 0; r<rotations; r++){
         cxt.save();
         cxt.translate(space*r,0)
-        cxt.beginPath();
-       // cxt.rect(0,0,64,64)
-        cxt.stroke();
-        cxt.closePath();
         cxt.translate(width/2,height/2)
         cxt.rotate(Math.PI*2*(r/rotations))
         cxt.translate(-width/2,-height/2)
         cxt.drawImage(c, 0, 0, width, height, 0,0,width,height)
         cxt.restore()
     }
+    };
+    img.src = file;
+
+
+
     this.canvas = c;
     this.context = cxt;
 }
 
 Sprites.prototype.renderToContextWithAngle = function(context, theta){
-    var index = Math.floor(rotations * (theta / 360));
-    
+    var index = Math.floor(this.rotations * (theta / 360));
+    context.save();
+    context.translate(-this.width/2, -this.height/2);
+    context.drawImage(this.canvas, index*this.space, 0, this.width, this.height, 0, 0, this.width, this.height);
+    context.restore();
 };
-
