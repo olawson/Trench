b6eed3b8a782ed6054901d9e03368d6542e3d0a1
Mark Bolusmjak <mark.bolusmjak@uken.com>
2011-09-23 23:26:11 -0400

    fixes.

--- src/soldier.js
+++ src/soldier.js
@@ -17,27 +17,41 @@ Soldier.prototype.radius = 10;
 Soldier.prototype.x = 0;
 Soldier.prototype.y = 0;
 Soldier.prototype.dead = true;
+Soldier.prototype.classification = MachineGunner;
 
 Soldier.prototype.direction = 45;
-Soldier.prototype.sprites = SpriteSets.allies;
+Soldier.prototype.sprites = SpriteSets.axis;
 
 
 Soldier.prototype.render = function(context){
     context.save();
-    context.beginPath();
     //translate to soldier-center = 0,0
     context.translate(this.x,this.y);
+    context.beginPath();
 
-    this.sprites.renderToContextWithAngle(context, this.direction)
-    //draw center based stuff
-    context.arc(0,0, this.radius, 0, Math.PI*2);
-
-    context.moveTo(0,0);
     var theta = this.direction * Math.PI/180;
-    context.lineTo(Math.cos(theta)*this.radius, Math.sin(theta)*this.radius)
+    var delta = this.classification.angle * Math.PI/180;
+    context.moveTo(0,0);
+    var r = this.classification.radius;
+    var x1 = Math.cos(theta-delta/2)*r;
+    var y1 = Math.sin(theta-delta/2)*r;
+    var x2 = Math.cos(theta+delta/2)*r;
+    var y2 = Math.sin(theta+delta/2)*r;
+    context.lineTo(x1, y1);
+    context.arc(0,0,r,theta-delta/2, theta+delta/2);
+//    context.arcTo(x1, y1, x2, y2, r);
+    context.lineTo(0,0);
+    context.lineTo(x2, y2);
+    context.closePath()
     context.stroke();
 
-    context.closePath();
+
+//    context.lineTo(0,0);
+    this.sprites.renderToContextWithAngle(context, this.direction)
+    //draw center based stuff
+
+
+
     context.restore();
 };
 
@@ -76,15 +90,15 @@ function Sprites(file, x, y, width, height, rotations, space){
     var img = new Image();
     img.onload = function(){
         cxt.drawImage(img, x, y, width, height, 0,0,width,height)
-    for (var r = 0; r<rotations; r++){
-        cxt.save();
-        cxt.translate(space*r,0)
-        cxt.translate(width/2,height/2)
-        cxt.rotate(Math.PI*2*(r/rotations))
-        cxt.translate(-width/2,-height/2)
-        cxt.drawImage(c, 0, 0, width, height, 0,0,width,height)
-        cxt.restore()
-    }
+        for (var r = 0; r<rotations; r++){
+            cxt.save();
+            cxt.translate(space*r,0)
+            cxt.translate(width/2,height/2)
+            cxt.rotate(Math.PI*2*(r/rotations))
+            cxt.translate(-width/2,-height/2)
+            cxt.drawImage(c, 0, 0, width, height, 0,0,width,height)
+            cxt.restore()
+        }
     };
     img.src = file;
 
--- src/soldier_type.js
+++ src/soldier_type.js
@@ -1,11 +1,14 @@
 var GenericSoldier = {
-  
+    radius : 20,
+    angle : 180
 };
 
 var Sniper = {
-  
+    radius : 60,
+    angle : 15
 };
 
 var MachineGunner = {
-  
+    radius : 140,
+    angle : 40
 };
